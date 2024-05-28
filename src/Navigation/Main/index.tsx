42dc8fbeeafdfbbb3aab73507dbebab83540a623
DanteBartel <dantedbartel@gmail.com>
2024-05-28 17:49:40 +0700

    240528 1749; Nhan; Customize tab bar

--- src/Navigation/Main/index.tsx
+++ src/Navigation/Main/index.tsx
@@ -8,6 +8,12 @@ import { MeContainer } from "@/Screens/Me";
 import { useDispatch } from "react-redux";
 import { setAccessToken, getAccessToken } from "@/Store/reducers";
 
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons/faHouse'
+import { faFile } from '@fortawesome/free-solid-svg-icons/faFile'
+import { faChartPie } from '@fortawesome/free-solid-svg-icons/faChartPie'
+import { faPerson } from '@fortawesome/free-solid-svg-icons/faPerson'
+
 const Tab = createBottomTabNavigator();
 
 // @refresh reset
@@ -27,37 +33,51 @@ export const MainNavigator = () => {
   }, [dispatch])
 
   return (
-    <Tab.Navigator>
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let icon = faHouse
+          if (route.name === "Home") {
+            icon = faHouse
+          } else if (route.name === "Record") {
+            icon = faFile
+          } else if (route.name === "Chart") {
+            icon = faChartPie
+          } else if (route.name === "Me") {
+            icon = faPerson
+          }
+          return <FontAwesomeIcon icon={icon} color={color} />
+        },
+        tabBarActiveTintColor: '#86D09D',
+        tabBarInactiveTintColor: 'gray'
+      })}
+    >
       <Tab.Screen
         name="Home"
         component={HomeContainer}
         options={{
-          tabBarIconStyle: { display: "none" },
-          tabBarLabelPosition: "beside-icon",
+          tabBarLabelPosition: "below-icon",
         }}
       />
       <Tab.Screen
         name="Record"
         component={RecordContainer}
         options={{
-          tabBarIconStyle: { display: "none" },
-          tabBarLabelPosition: "beside-icon",
+          tabBarLabelPosition: "below-icon",
         }}
       />
       <Tab.Screen
         name="Chart"
         component={ChartContainer}
         options={{
-          tabBarIconStyle: { display: "none" },
-          tabBarLabelPosition: "beside-icon",
+          tabBarLabelPosition: "below-icon",
         }}
       />
       <Tab.Screen
         name="Me"
         component={MeContainer}
         options={{
-          tabBarIconStyle: { display: "none" },
-          tabBarLabelPosition: "beside-icon",
+          tabBarLabelPosition: "below-icon",
         }}
       />
     </Tab.Navigator>
